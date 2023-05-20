ddd479cc7900e025266f3b17ee4975b7ca82abf7
UChamika <118790869+ChamikaUluwatta@users.noreply.github.com>
2023-05-20 13:58:13 +0530

    Add styles for the TaskForm Component

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -4,16 +4,14 @@ const TaskForm = (props) => {
   const [taskName, setTaskName] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
   const [taskDate, setTaskDate] = useState("");
-  const [taskTime, setTaskTime] = useState("");
-  const [taskDuration, setTaskDuration] = useState("");
+  const [taskDuration, setTaskDuration] = useState("1");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const taskData = {
       taskName: taskName,
       taskDescription: taskDescription,
-      taskDate: taskDate,
-      taskTime: taskTime,
+      taskDate: new Date(taskDate),
       taskDuration: taskDuration,
     };
     props.onsubmitForm(taskData);
@@ -21,47 +19,92 @@ const TaskForm = (props) => {
     setTaskDescription("");
     setTaskDuration("");
     setTaskName("");
-    setTaskTime("");
+  };
+
+  const handleTaskNameChange = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  const handleTaskDescriptionChange = (event) => {
+    setTaskDescription(event.target.value);
+  };
+
+  const handleTaskDateChange = (event) => {
+    setTaskDate(event.target.value);
+  };
+
+  const handleTaskDurationChange = (event) => {
+    setTaskDuration(event.target.value);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex justify-center items-center mt-6 flex-col">
-          <div class="z-0 w-1/4 mb-6 flex justify-center items-center">
+    <div className="flex justify-center rounded-lg items-center mt-3">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-700 w-1/2 h-auto  rounded-lg shadow-xl dark:shadow-gray-800"
+      >
+        <div className="flex justify-center items-center mt-6 flex-col bg-gray-650">
+          <div class="z-0 w-1/4 mb-6 flex justify-center items-center bg-gray-700 ">
             <input
               type="text"
-              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
+              class="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              value={taskName}
               required
+              onChange={handleTaskNameChange}
             />
             <label
               for="floating_task_name"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-6 -z-10 origin-center peer-focus:origin-center peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-2"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-8 -z-10 origin-center peer-focus:origin-center peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-2"
             >
               Task name
             </label>
           </div>
-          <div class="z-0 w-1/4 mb-6 mt-6 flex justify-center items-center">
+          <div class="z-0 w-1/4 mb-6 mt-6 flex justify-center items-center bg-gray-700">
             <textarea
-              class="resize-y block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
+              class="resize-y block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              value={taskDescription}
+              onChange={handleTaskDescriptionChange}
               required
             />
             <label
               for="floating_task_description"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-50 -z-10 origin-center peer-focus:origin-center peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-7  peer-focus:top-11"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-7 scale-75 top-50 -z-10 origin-center peer-focus:origin-center peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-12  peer-focus:top-11"
             >
               Task description
             </label>
           </div>
 
-          
+          <div class="flex flex-col mt-3 mb-6 bg-gray-700">
+            <input
+              type="date"
+              class=" font-semibold bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={taskDate}
+              onChange={handleTaskDateChange}
+            ></input>
+            <div class="z-0 flex justify-center items-center mt-2 mb-2 ">
+              <label class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+                Start Date
+              </label>
+            </div>
+          </div>
+          <div class="flex flex-row h-10 rounded-lg relative bg-transparent mt-1 justify-center mb-4 w-1/4 items-center">
+            <input
+              type="number"
+              className=" font-semibold bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              min="1"
+              value={taskDuration}
+              onChange={handleTaskDurationChange}
+            ></input>
+          </div>
+          <div class="z-0 flex justify-center items-center  mb-2 ">
+            <label class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+              Duration (hours)
+            </label>
+          </div>
         </div>
-
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-1/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="mb-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-1/4 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
